102209e48f324afbe72bbe1caed55fda2de12398
Fernando <fernandomaldaner15@gmail.com>
2024-02-29 19:49:07 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Avatar - Modo Claro")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Avatar - Modo Escuro")
+  }
+}
